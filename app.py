9eda9249e4e8d9c5e55990725a1e1823161933c7
Jye Robinson <jye@jrobi.co>
2020-08-01 13:12:47 +1000

    Added maze background and scaled to screen

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import pygame, sys
 
-width = 600
-height = 600
+pygame.init()
+width = 560
+height = 620
 
 class App:
     def __init__(self):
         self.screen = pygame.display.set_mode((width, height))
         self.running = True
+        self.load()
+        self.state = 1 #1=playing 0=game over
+        
 
     def run(self):
         while self.running:
-            ##
+            if self.state = 1:
+                self.draw()
+            else:
+                self.running = False
         pygame.quit()
         sys.exit()
     
@@ -18,4 +25,12 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+    
+    def load(self):
+        self.background = pygame.image.load('maze.png')
+        self.background = pygame.transform.scale(self.background, (width, height))
+    
+    def draw(self):
+        self.screen.blit(self.background, (0, 0))
+        pygame.display.update()
 

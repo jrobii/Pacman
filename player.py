9e04a3587fcc00df608559bdc10a7400f3d28b57
Jye Robinson <jye@jrobi.co>
2020-08-05 13:34:22 +1000

    Added functions to check whether a player is moving within the centre of the grid.

--- player.py
+++ player.py
@@ -8,23 +8,36 @@ class Player:
         self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
         self.dir = vec(1, 0)
+        self.stored_dir = None
         
     
     def get_pix_pos(self):
         return vec((self.grid_pos.x*self.app.cell_width)+self.app.cell_width//2, 
         (self.grid_pos.y*self.app.cell_height)+self.app.cell_height//2)
-        print(self.grid_pos, self.pix_pos)
+        
     
     def update(self):
         self.pix_pos += self.dir
+        self.check_legal_move_x()
+        self.check_legal_move_y()
         self.grid_pos.x = (self.pix_pos.x)//self.app.cell_width
         self.grid_pos.y = (self.pix_pos.y)//self.app.cell_height
+
+    def check_legal_move_x(self):
+        if int(self.pix_pos.x+self.app.cell_width//2) % self.app.cell_width == 0:
+            if self.dir == vec(1, 0) or self.dir == vec(-1, 0):
+                if self.stored_dir != None:
+                    self.dir = self.stored_dir
+    
+    def check_legal_move_y(self):
+        if int(self.pix_pos.y+self.app.cell_height//2) % self.app.cell_height == 0:
+            if self.dir == vec(0, 1) or self.dir == vec(0, -1):
+                if self.stored_dir != None:
+                    self.dir = self.stored_dir
 
     def draw(self):
         pygame.draw.circle(self.app.screen, yellow, (int(self.pix_pos.x),
         int(self.pix_pos.y)), self.app.cell_width//2-2)
 
     def move(self, dir):
-        self.dir = dir
-
-
+        self.stored_dir = dir 
